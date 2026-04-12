455b4837321de0cfad50e0af8a8adc5dfaf48edb
Sonu Kumar <krsonu025@gmail.com>
2026-04-12 17:20:45 +0530

    import and export in component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  
+  return (
+    <>
+      <h1>hello react 19 </h1>
+      <Fruit />
+      
+    </>
+  )
+}
+
+function Fruit() {
+  return <div>apple</div>
+}
