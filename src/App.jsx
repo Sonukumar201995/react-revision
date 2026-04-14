92092a6b731ffca37ca74847be16bcdf74a56e70
Sonu Kumar <krsonu025@gmail.com>
2026-04-14 11:52:21 +0530

    State in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-export default function App()
-{
-  let name="sonu kumar"
-  return(
-    <>
-    <h2>jsx in react {name}</h2>
-    <button onClick={()=>alert("hello")}> click</button>
-    </>
-  )
+import { useState } from "react";
+
+function App() {
+  const [fruit, setFruit] = useState("Apple");
+  const handleFruit = () => {
+    setFruit("Banana")
+  }
+  return (
+    <div>
+      <h1>State in React</h1>
+      <h1>{fruit}</h1>
+      <button onClick={handleFruit}>change name</button>
+    </div>
+  );
 }
+
+export default App;
