8283d30a269834b3abac4cc59ba6ed9148ed67c6
Sonu Kumar <krsonu025@gmail.com>
2026-04-15 13:41:32 +0530

    props in react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from "react"
+import User from "./User"
 
 export default function App()
 {
-  const[count,setCount]=useState(0);
   return(
     <div>
-      <h1>{count}</h1>
-      <button onClick={()=>setCount(count+1)}>Counter</button>
-      {
-        count==0 ? <h1>condition 0</h1>:
-        count==1 ? <h1>condition 1</h1>:
-        count==2 ? <h1>condition 2</h1>:
-        count==3 ? <h1>condition 3</h1>:
-        count==4 ? <h1>condition 4</h1>:
-        <h1>other condition</h1>
-      }
+      <h1>props in react js</h1>
+      <User name="sonu kumar" age='31'/>
     </div>
   )
 }
---
+++ src/User.jsx
@@ -0,0 +1,8 @@
+export default function User({name,age})
+{
+    return(
+        <div>
+            <h1>Name :{name} {age}</h1>
+        </div>
+    )
+}
