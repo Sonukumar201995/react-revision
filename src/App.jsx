3c30277c9b1d7e80a1d91b0d218ed74a0ee97ee9
Sonu Kumar <krsonu025@gmail.com>
2026-04-16 16:25:19 +0530

    controlled component in react

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from "react"
 
 export default function App()
 {
-  const[val,setVal]=useState("")
+  const[name,setName]=useState("")
+  const[email,setEmail]=useState("")
+  const[address,setAddress]=useState("")
   return(
     <div>
       <h2>Get input field value</h2>
-      <input type="text" value={val} onChange={(event)=>setVal(event.target.value)} placeholder="Enter Your Name"/>
-      <h2>{val}</h2>
-      <button onClick={()=>setVal("")}>clear</button>
+      <input type="text" value={name} onChange={(event)=>setName(event.target.value)} placeholder="Enter Your Name"/>
+      <br /><br />
+      <input type="text" value={email} onChange={(event)=>setEmail(event.target.value)} placeholder="Enter Your Email"/>
+      <br /><br />
+      <input type="text" value={address} onChange={(event)=>setAddress(event.target.value)} placeholder="Enter Your Address"/>
+      <br />
+      <br />
+      <button>submit</button>
+      <button onClick={()=>{setName(""),setEmail(""),setAddress("")}}>clear</button>
+
+      <h2>{name}</h2>
+      <h2>{email}</h2>
+      <h2>{address}</h2>
     </div>
   )
 }
