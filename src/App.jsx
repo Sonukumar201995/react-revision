cefed7360acdf9e52c7ff41803dba9b65e2649c4
Sonu Kumar <krsonu025@gmail.com>
2026-04-18 14:01:20 +0530

    handle radion and dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import Skills from "./Skills";
+import { useState } from "react"
 
 export default function App()
 {
+
+  const[gender,setGender]=useState("male")
+  const[city,setCity]=useState("Delhi")
   return(
     <div>
-      <h1>Handle Check Box</h1>
-      <Skills/>
+      <h2>Handle Radio and Dropdown</h2>
+      <h4>Select Gender: {gender}</h4>
+      <input type="radio"  onChange={(event)=>setGender(event.target.value)} 
+       checked={gender=="male"} id="male" name="gender" value={"male"}/>
+      <label htmlFor="male">Male</label>
+
+      <input onChange={(event)=>setGender(event.target.value)} 
+      checked={gender=="female"} type="radio" id="female" name="gender" value={"female"} />
+      <label htmlFor="female">Female</label>
+      
+
+      <h2>select city: {city}</h2>
+      <select onChange={(event)=>setCity(event.target.value)} defaultValue={"Delhi"}>
+        <option value="Delhi">Delhi</option>
+        <option value="Noida">Noida</option>
+        <option value="Gurugram">Gurugram</option>
+      </select>
     </div>
   )
 }
