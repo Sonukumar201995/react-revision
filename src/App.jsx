d024bbb3e13e545af69fc75f921de88f1164b7ab
Sonu Kumar <krsonu025@gmail.com>
2026-04-19 15:31:46 +0530

    jsx loop in react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import { useState } from "react"
+export default function App() {
+  const userData = [
+    { name: 'sonu kumar', age: 30, email: 'krsonu@gmail.com', id: 1 },
+    { name: "monu kumar", age: 28, email: 'krmonu@gmail.com', id: 2 },
+    { name: 'arun kumar', age: 30, email: 'krarun@gmail.com', id: 3 }
+  ];
 
-export default function App()
-{
-
-  const[gender,setGender]=useState("male")
-  const[city,setCity]=useState("Delhi")
-  return(
+  return (
     <div>
-      <h2>Handle Radio and Dropdown</h2>
-      <h4>Select Gender: {gender}</h4>
-      <input type="radio"  onChange={(event)=>setGender(event.target.value)} 
-       checked={gender=="male"} id="male" name="gender" value={"male"}/>
-      <label htmlFor="male">Male</label>
-
-      <input onChange={(event)=>setGender(event.target.value)} 
-      checked={gender=="female"} type="radio" id="female" name="gender" value={"female"} />
-      <label htmlFor="female">Female</label>
-      
-
-      <h2>select city: {city}</h2>
-      <select onChange={(event)=>setCity(event.target.value)} defaultValue={"Delhi"}>
-        <option value="Delhi">Delhi</option>
-        <option value="Noida">Noida</option>
-        <option value="Gurugram">Gurugram</option>
-      </select>
+      <h2>jsx loop in react js</h2>
+      <table border='1'>
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Email</td>
+          </tr>
+        </thead>
+        <tbody>
+          {userData.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.age}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
